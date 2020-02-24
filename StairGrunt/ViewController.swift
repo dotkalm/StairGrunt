0d0c11b9f482b99613a68779cbfd7d03fd737a78
dotkalm <joel@joelholmberg.com>
2020-02-24 10:54:50 -0800

    Initial Commit

---
+++ StairGrunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StairGrunt
+//
+//  Created by Joel on 2/24/20.
+//  Copyright © 2020 dotkalm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
